52364a25f75b6905bf38e87b845d737bae5a0e73
V2V2V2-droid <74412016+V2V2V2-droid@users.noreply.github.com>
2022-03-01 13:56:45 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from datetime import datetime as dt
+
+now = dt.now().strftime('%Y-%b-%d')
+print(now)
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('PyCharm')
+
+
