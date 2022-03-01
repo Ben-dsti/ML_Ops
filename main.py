8ee4b88167be4220c47d7ec0518a1c7cd1e33df5
Ms Vigot <victoria.vigot@gmail.com>
2022-03-01 14:03:33 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ print(now)
 
 print(now + "date today")
 
+print("See into this branch : branch main")
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print('PyCharm')
